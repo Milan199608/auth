7638b4ebf0860547e7d741348abd9089f10e18c9
Milan199608 <executive100007@gmail.com>
2021-09-30 16:43:14 +0530

    auth started

--- Server/Server.js
+++ Server/Server.js
@@ -1,10 +1,30 @@
-
+const mongoose= require("mongoose");
 const express = require("express");
 const app = express();
-  
+
+
+const DB ="mongodb+srv://Milan:milan99@$@cluster0.ejniy.mongodb.net/auth?retryWrites=true&w=majority";
+ 
+
+mongoose.connect(DB,{
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}).then(()=>{
+  console.log("connection successfull");
+
+}).catch((error)=>
+console.log(`no connection`)
+);
+
+const middleware=(req,res,next)=>{
+  console.log(`hello from middleware`);
+  next();
+
+}
+
 app.get("/", (req, res) => {
 
-  res.send("connected");
+  res.send("connetdhhsdthdsght");
 
 });
   
